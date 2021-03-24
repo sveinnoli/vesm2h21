9842926362b2de7f05f2a6ef3c79a25e8dada6cc
sveinnoli <46562494+sveinnoli@users.noreply.github.com>
2021-03-24 22:18:33 +0000

    Add files via upload

---
+++ Verkefni2/Code/RGB_LED_HEX.ino
@@ -0,0 +1,40 @@
+/*
+Adafruit Arduino - Lesson 3. RGB LED
+*/
+ 
+int redPin = 11;
+int greenPin = 10;
+int bluePin = 9;
+const int ldrPin = A0;
+
+int rgbBrightness;
+float ldrPercentage;
+void setup()
+{
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);  
+  pinMode(ldrPin, INPUT);
+  Serial.begin(9600);
+}
+ 
+void loop()
+{
+  float ldrStatus = analogRead(ldrPin);
+  ldrPercentage = ldrStatus/400;
+  if (ldrPercentage > 1) {
+    ldrPercentage = 1; 
+  }
+  rgbBrightness = 255*ldrPercentage;
+  Serial.print("\n");
+  setColor(rgbBrightness, rgbBrightness, rgbBrightness);  // red
+  delay(1000);
+  
+}
+ 
+void setColor(int red, int green, int blue)
+{
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);  
+}
