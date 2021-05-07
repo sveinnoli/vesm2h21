18c0611803299496b4d1aaccbcdce31b2dc5a816
sveinnoli <46562494+sveinnoli@users.noreply.github.com>
2021-05-07 20:46:30 +0000

    cleanup

--- Lokaverkefni/transmitter/transmitter.ino
+++ Lokaverkefni/transmitter/transmitter.ino
@@ -155,6 +155,8 @@ void loop()
   joystick_button_state = digitalRead(joystick_button);
   joystick.update_data(x_axis, y_axis, joystick_button_state);
   joystick.print_data();
+
+  
   //MPU 6050 sensors
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
