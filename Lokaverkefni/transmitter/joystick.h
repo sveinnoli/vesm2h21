9a3e6b9e8ba94d89c2a47dc57ba18677f844b7fb
Sveinn oli Gudmundsson <sveinnoli@hotmail.com>
2021-05-07 19:56:20 +0000

    added joystick class

---
+++ Lokaverkefni/transmitter/joystick.h
@@ -0,0 +1,23 @@
+/*
+    Sveinn Oli, G.
+    Joystick library used to interact with the joystick and return directional control.
+*/
+-
+#ifndef Joystick_h
+#define Joystick_h
+
+#include "Arduino.h"
+
+
+class Joystick
+{
+    public:
+        Joystick(int x, int y, bool button_state);
+        void print_data()
+    private:
+        int _x;
+        int _y;
+        int _button_state;
+}
+
+#endif
